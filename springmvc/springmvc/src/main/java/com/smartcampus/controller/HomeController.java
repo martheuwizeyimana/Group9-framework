932adcb304fd44bd6e467dcc390b99a1d59fcd33
josenarame-cmd <josenarame@gmail.com>
2026-02-25 17:15:20 +0200

    Enhance HomeController with service injections and home method
    Added services to HomeController and implemented home method to populate model attributes.

--- springmvc/springmvc/src/main/java/com/smartcampus/controller/HomeController.java
+++ springmvc/springmvc/src/main/java/com/smartcampus/controller/HomeController.java
@@ -1,6 +1,14 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
 package com.smartcampus.controller;
 
-import com.smartcampus.service.*;
+import com.smartcampus.service.CourseService;
+import com.smartcampus.service.DepartmentService;
+import com.smartcampus.service.LecturerService;
+import com.smartcampus.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,6 +16,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+    @Autowired
+    private StudentService studentService;
+    @Autowired
+    private LecturerService lecturerService;
+    @Autowired
+    private CourseService courseService;
+    @Autowired
+    private DepartmentService departmentService;
 
-
+    @GetMapping({"/dashboard"})
+    public String home(Model model) {
+        model.addAttribute("studentCount", this.studentService.count());
+        model.addAttribute("lecturerCount", this.lecturerService.count());
+        model.addAttribute("courseCount", this.courseService.count());
+        model.addAttribute("departmentCount", this.departmentService.count());
+        return "home";
+    }
 }
